4c733f97b8455a669ddf72dd099a72ee027c4812
tyrpo <154536175+tyrpo@users.noreply.github.com>
2024-07-04 16:52:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -109,12 +109,3 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
 
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
