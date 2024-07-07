28d7d264063fc996a8dfe22b2bb6ff91d0933105
tyrpo <154536175+tyrpo@users.noreply.github.com>
2024-07-07 16:06:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
-// let btn5 = document.getElementById("btn5");
-// let btn6 = document.getElementById("btn6");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 let btn7 = document.getElementById("btn7");
 let btn8 = document.getElementById("btn8");
 
@@ -60,27 +60,27 @@ btn4.addEventListener("click", function(){
 	}
 });
 
-// btn5.addEventListener("click", function(){
-// 	if (tg.MainButton.isVisible) {
-// 		tg.MainButton.hide();
-// 	}
-// 	else {
-// 		tg.MainButton.setText("Нажмите, чтобы оплатить или узнать больше!");
-// 		item = "5";
-// 		tg.MainButton.show();
-// 	}
-// });
+btn5.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Нажмите, чтобы оплатить или узнать больше!");
+		item = "5";
+		tg.MainButton.show();
+	}
+});
 
-// btn6.addEventListener("click", function(){
-// 	if (tg.MainButton.isVisible) {
-// 		tg.MainButton.hide();
-// 	}
-// 	else {
-// 		tg.MainButton.setText("Нажмите, чтобы оплатить или узнать больше!");
-// 		item = "6";
-// 		tg.MainButton.show();
-// 	}
-// });
+btn6.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Нажмите, чтобы оплатить или узнать больше!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
 
 btn7.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
